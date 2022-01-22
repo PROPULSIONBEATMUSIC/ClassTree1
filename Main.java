7d6abf17bf58d76197858d444f6793a6a2ff80d8
PROPULSIONBEATMUSIC <74822351+PROPULSIONBEATMUSIC@users.noreply.github.com>
2022-01-22 12:55:14 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,26 +1,91 @@
-import java.io.*;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.Method;
 
-public class Main {
+public class Car {
+    int speed;
+    int cost;
+    String owner;
+
+    public Car(int speed, int cost, String owner) {
+        this.speed = speed;
+        this.cost = cost;
+        this.owner = owner;
+    }
+
+    public static void showInfo(){
+        System.out.println("Car");
+    }
+
     public static void main(String[] args) {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("person.dat"))){
-            Person p = new Person("Sam", 33, 178, true);
-            oos.writeObject(p);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
+        Car car = new Car(100, 100000, "Mr.E");
+        final Method[] methods = car.getClass().getDeclaredMethods();
+        System.out.println(methods[0]);
+        Car.showInfo();
+    }
+
+    static class Truck extends Car {
+        public Truck(int speed, int cost, String owner) {
+            super(speed, cost, owner);
+        }
+
+        public static void showInfo(){
+            System.out.println("Truck");
+        }
+
+        public static void main(String[] args) {
+            try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("file.txt"))) {
+                Truck truck = new Truck(50, 20000, "Mr.C");
+                final Method[] methods = truck.getClass().getDeclaredMethods();
+                System.out.println(methods[0]);
+                oos.writeObject(truck);
+            } catch (Exception ex){
+                System.out.println(ex.getMessage());
+            }
+            Truck truck = new Truck(50, 20000, "Mr.C");
+            final Method[] methods = truck.getClass().getDeclaredMethods();
+            Truck.showInfo();
         }
     }
-}
 
-class Person implements Serializable{
-    private String name;
-    private int age;
-    private double height;
-    private boolean married;
+    static class Supercar extends Car{
+        public Supercar(int speed, int cost, String owner) {
+            super(speed, cost, owner);
+        }
 
-    Person(String n, int a, double h, boolean m){
-        name = n;
-        age = a;
-        height = h;
-        married = m;
+        public static void showInfo(){
+            System.out.println("Supercar");
+        }
+
+        public static void main(String[] args) {
+            try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("file.txt"))) {
+                Supercar supercar = new Supercar(200, 1000000, "Mr.F");
+                final Method[] methods = supercar.getClass().getDeclaredMethods();
+                System.out.println(methods[0]);
+                oos.writeObject(supercar);
+            } catch (Exception ex){
+                System.out.println(ex.getMessage());
+            }
+            Supercar.showInfo();
+        }
+    }
+    public static class pCar extends Supercar{
+        public pCar(int speed, int cost, String owner) {
+            super(speed, cost, owner);
+        }
+        public static void showInfo(){
+            System.out.println("pCar");
+        }
+        public static void main(String[] args) {
+            try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("file.txt"))) {
+                pCar pCar = new pCar(13213212,31232132,"Mr.j");
+                final Method[] methods = pCar.getClass().getDeclaredMethods();
+                System.out.println(methods[0]);
+                oos.writeObject(pCar);
+            } catch (Exception ex){
+                System.out.println(ex.getMessage());
+            }
+            pCar.showInfo();
+        }
     }
 }
